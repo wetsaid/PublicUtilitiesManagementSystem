a5089bcfd5c10037d26ef233acc0acf70d256a3d
wetsaid <470348641@qq.com>
2017-03-11 16:17:20 +0800

    Hello world!

---
+++ src/main/java/cn/edu/zucc/shijf/dao/TUserMapper.java
@@ -0,0 +1,31 @@
+package cn.edu.zucc.shijf.dao;
+
+import cn.edu.zucc.shijf.model.TUser;
+import cn.edu.zucc.shijf.model.TUserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TUserMapper {
+    int countByExample(TUserExample example);
+
+    int deleteByExample(TUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TUser record);
+
+    int insertSelective(TUser record);
+
+    List<TUser> selectByExample(TUserExample example);
+
+    TUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByExample(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+}
